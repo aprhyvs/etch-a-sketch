cf4e49e2475e9f2b4ff837cf52f630fc828c6ab2
aprhyvs <173691141+aprhyvs@users.noreply.github.com>
2025-07-18 20:15:12 +0800

    add "hover" effect

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ for (i = 0; i < (gridNumber * gridNumber); i++){
   gridItem.textContent = `${i}`
   gridItem.classList.add("grid-item")
 }
+
+const gridItemEl = document.querySelectorAll(".grid-item")
+
+gridItemEl.forEach(element => {
+  element.addEventListener("mouseover", event =>{
+    event.target.classList.add("change")
+  });
+});
+
+/*gridItemEl.forEach(element => {
+  element.addEventListener("mouseout", event =>{
+    event.target.classList.remove("change")
+  });
+});*/
+

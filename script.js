057140d9ee4c5c0b77280ba2a122f73e476c7777
aprhyvs <173691141+aprhyvs@users.noreply.github.com>
2025-07-20 15:07:09 +0800

    add feature to check if grid-item has color
    this is to prevent the grid-item color being replaced if it already has
    a color, which should help in implementing the darkening effect

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let gridNumber = 16
 let gridContainer = document.createElement("div")
 let setBtn = document.createElement("button")
+let minOpacity = 0.1
 
 //extra credit #1
 function rgb(){
@@ -8,6 +9,7 @@ function rgb(){
   let blue = Math.floor(Math.random() * 256); 
   let green = Math.floor(Math.random() * 256);
 
+// console.log(`rgb(${red}, ${blue}, ${green}, ${minOpacity})`)
   return `rgb(${red}, ${blue}, ${green})`
 }
 
@@ -22,6 +24,8 @@ setBtnEl.addEventListener("click", () => {
   
   if (setGridNumber > 100){
     alert("no you can't do more than a 100")
+  } else if (setGridNumber == 0 || setGridNumber == null){
+    alert("no you can't make it blank")
   } else {
     gridNumber = setGridNumber;
     setGrid()
@@ -38,23 +42,45 @@ function setGrid(){
   for (i = 0; i < (gridNumber * gridNumber); i++){
     let gridItem = document.createElement("div")
     gridContainer.appendChild(gridItem);
-    gridItem.textContent = `${i}`
+    //gridItem.textContent = `${i}`
     gridItem.classList.add("grid-item")
   }  
 
   const gridItemEl = document.querySelectorAll(".grid-item")
+  const rgbEl = document.querySelectorAll(".rgb")
 
   gridItemEl.forEach(element => {
     element.style.width = (512 / gridNumber) + "px";
     element.style.height = (512 / gridNumber) + "px";
-    element.style.border = "0.5px solid black";
+    element.style.border = "0.5px solid rgb(0,0,0, 0.25)";
   })
 
   gridItemEl.forEach(element => {
-    element.addEventListener("mouseover", event =>{
-      event.target.style.backgroundColor = rgb();
+    element.addEventListener("mouseenter", event =>{
+      if (!event.target.classList.contains("rgb")){
+        event.target.style.backgroundColor = rgb();
+      } else {
+        console.log("this grid item already has a color")
+      }
+      
+      event.target.classList.add("rgb");        
+      
+      /* trying to implement darkening effect
+      element.addEventListener("mouseenter", event => {
+        event.target.style.opacity = minOpacity;
+        if(minOpacity < 1) {
+          minOpacity += 0.1;  
+        }
+      });*/
     });
   });
+
+  /*
+  if (gridItemEl.classList.contains("rgb  ")){
+    console.log("a element with rgb exists.")
+  } else {
+    console.log("nothing")
+  }*/
 } 
 
 function clearGrid(){
@@ -63,4 +89,6 @@ function clearGrid(){
   while(removeGrid.length > 0){
     removeGrid[0].parentNode.removeChild(removeGrid[0]);
   }
+
+  minOpacity = 0.1
 }
